b6ee2703eed88c1ed420009b040067e4db3b8edd
daniloagostinhosilva@gmail.com <danilosilva@MacBook-Air-de-Danilo.local>
2019-12-03 22:00:10 -0300

    api com funcoes basicas finalizada

--- server.js
+++ server.js
@@ -3,24 +3,35 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
-const id_json = require('./json/id_json.json')
-const produtos = [
-  {id: 1, nome: 'cafeteira'},
-  {id: 2, nome: 'fruteira'}
-]
+const id_json = require('./json/id_json.json');
 
 app.get('/', (req, res) => {
   res.send(id_json);
 });
-
+app.get('/sites/MLA/search', (req, res) => {
+  const productResultQuery = id_json.find(c => c.item.description === req.query.q);
+  if(!productResultQuery) res.status(404).send('O produto que voce procura nao existe!')
+  res.send(productResultQuery);
+});
 app.get('/items/:id', (req, res) => {
-  const productResult = id_json.find(c => Number(c.item.id) === Number(req.params.id));
-  if(!productResult) res.status(404).send('O produto que voce procura nao existe!')
-  res.send(productResult);
+  const productResultId = id_json.find(c => Number(c.item.id) === Number(req.params.id));
+  if(!productResultId) res.status(404).send('O produto que voce procura nao existe!')
+  res.send(productResultId);
 });
 
 app.get('/items/:id/description', (req, res) => {
-  res.send(id_json);
+  const productResultDescription = id_json.find(c => c.item.description === req.params.id);
+  if(!productResultDescription) res.status(404).send('O produto que voce procura nao existe!')
+  res.send(productResultDescription);
 });
+
+app.use((req, res, next) => {
+  res.setHeader('Acess-Control-Allow-Origin', '*');
+  res.setHeader('Acess-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Acess-Control-Allow-Headers', 'X-Requested-With, content-type');
+  res.setHeader('Acess-Control-Allow-Credentials', true);
+
+  next();
+})
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
