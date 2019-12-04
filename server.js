6eaad8d0404e8b64b7362d0c3567ef7c1e3a9e77
daniloagostinhosilva@gmail.com <danilosilva@MacBook-Air-de-Danilo.local>
2019-12-03 22:34:30 -0300

    criando segundo arquivo json com response para query

--- server.js
+++ server.js
@@ -3,14 +3,18 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
-const id_json = require('./json/id_json.json');
+// busca por produtos
+const product = require('./json/product.json');
+
+// json detalhe do produto
+const id_json = require('./json/product_id.json');
 
 app.get('/', (req, res) => {
   res.send(id_json);
 });
 app.get('/sites/MLA/search', (req, res) => {
-  const productResultQuery = id_json.find(c => c.item.description === req.query.q);
-  if(!productResultQuery) res.status(404).send('O produto que voce procura nao existe!')
+  const productResultQuery = product.find(c => c.item.title === req.query.q);
+  // if(!productResultQuery) res.status(404).send('O produto que voce procura nao existe!')
   res.send(productResultQuery);
 });
 app.get('/items/:id', (req, res) => {
@@ -19,11 +23,6 @@ app.get('/items/:id', (req, res) => {
   res.send(productResultId);
 });
 
-app.get('/items/:id/description', (req, res) => {
-  const productResultDescription = id_json.find(c => c.item.description === req.params.id);
-  if(!productResultDescription) res.status(404).send('O produto que voce procura nao existe!')
-  res.send(productResultDescription);
-});
 
 app.use((req, res, next) => {
   res.setHeader('Acess-Control-Allow-Origin', '*');
