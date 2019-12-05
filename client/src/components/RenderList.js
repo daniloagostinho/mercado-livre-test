a8377ea17118188e7324f56c0428daf0109d18d5
daniloagostinhosilva@gmail.com <danilosilva@MacBook-Air-de-Danilo.local>
2019-12-05 16:36:22 -0300

    finalizando pagina detalhe do produto

--- client/src/components/RenderList.js
+++ client/src/components/RenderList.js
@@ -2,6 +2,9 @@ import React from 'react';
 import './RenderList.css';
 
 import Loader from '../loader2.png';
+
+import { Link } from "react-router-dom";
+
 class RenderList extends React.Component {
   constructor(props) {
     super(props);
@@ -11,20 +14,17 @@ class RenderList extends React.Component {
   }
   renderSearchResults = () => {
     let { results } = this.props.estadoSeach;
-    if (this.props.estadoSeach.isEmpty) {
-      results = "";
-    }
     if (Object.keys(results).length && results.length > 0) {
       return (
         <div className="results-container">
           {results.map((result) => {
             return (
               <div className="image-wrapper">
+              <Link to={`/items/${result.item.id}`}>aqui</Link>
                 <h3>{result.item.price.currency} {result.item.price.decimals}</h3>
                 <h4>{result.item.title}</h4>
-
-                <img className="image" src={result.item.picture} alt={result.user} />              </div>
-
+                <img className="image" src={result.item.picture} alt={result.user} />
+                </div>
             );
           })}
         </div>
@@ -35,7 +35,7 @@ class RenderList extends React.Component {
     return (
       <div>
         {this.renderSearchResults()}
-        <p>{this.props.estadoSeach.message}</p>
+        <p className="message-search">{this.props.estadoSeach.message}</p>
         <h2> {this.props.estadoSeach.loading}</h2>
         <img src={Loader} className={`search-loading ${this.props.estadoSeach.loading ? 'show' : 'hide'}`} alt="loader" />
       </div>
