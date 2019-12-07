efd002e41c49d69afaf64868db7c38604dec772e
daniloagostinhosilva@gmail.com <danilosilva@MacBook-Air-de-Danilo.local>
2019-12-07 15:50:32 -0300

    finalizando estilo e refator das rotas

--- client/src/components/RenderList.js
+++ client/src/components/RenderList.js
@@ -3,7 +3,7 @@ import './RenderList.css';
 
 
 import { Link } from "react-router-dom";
-
+import PostManImail from '../car_mail.png';
 class RenderList extends React.Component {
   constructor(props) {
     super(props);
@@ -13,12 +13,13 @@ class RenderList extends React.Component {
   }
   renderSearchResults = () => {
     let { results } = this.props.estadoSeach;
+    console.log('categorias >>', ((results.results || {}).categories || []) )
     if (Object.keys(results).length && results.length > 0) {
       return (
         <div className="results-container">
-          {(results).map((categorie) =>
+          {(results.categories || []).map((categorie) =>
             <ul className="menu">
-              <li key={categorie}>
+              <li key={JSON.stringify(categorie)}>
               </li>
             </ul>
           )}
@@ -26,6 +27,9 @@ class RenderList extends React.Component {
             return (
               <div className="image-wrapper">
                 <Link to={`/items/${result.item.id}`}><img className="image" src={result.item.picture} alt={result.user} /></Link>
+                {result.item.free_shipping &&
+                  <img className="car-mail-img" src={PostManImail} alt={result.user} />
+                }
                 <p class="price-tag">{result.item.price.currency} {result.item.price.decimals}</p>
                 <p>{result.item.title}</p>
                 <p class="aligth-right">Capital Federal</p>
@@ -43,8 +47,8 @@ class RenderList extends React.Component {
         <p className="message-search">{this.props.estadoSeach.message}</p>
         <h2> {this.props.estadoSeach.loading}</h2>
         {this.props.estadoSeach.loading &&
-          <p>
-            Buscando pelo produto...
+          <p className="loading">
+            Buscando por favor aguarde...
           </p>
         }
       </div>
--- server.js
+++ server.js
@@ -7,30 +7,9 @@ app.use(cors({
 }));
 const port = process.env.PORT || 5000;
 
-// busca por produtos
-const product = require('./json/product.json');
+const routes =  require('./routes/routes');
 
-// json detalhe do produto
-const id_json = require('./json/product_id.json');
-
-app.get('/', (req, res) => {
-  res.send(id_json);
-});
-const capitalize = (s) => {
-  if (typeof s !== 'string') return ''
-  return s.charAt(0).toUpperCase() + s.slice(1)
-}
-app.get('/sites/MLA/search', (req, res) => {
-  const productResultQuery = product.filter(c => c.item.title.startsWith(capitalize(req.query.q)));
-  if(productResultQuery == Array.isArray({})) res.status(404).send('O produto que voce procura nao existe!')
-  res.send(productResultQuery);
-});
-app.get('/items/:id', (req, res) => {
-  const productResultId = id_json.find(c => Number(c.item.id) === Number(req.params.id));
-  if(!productResultId) res.status(404).send('O produto que voce procura nao existe!')
-  res.send(productResultId);
-});
-
+app.use(routes);
 
 app.use((req, res, next) => {
   res.setHeader('Acess-Control-Allow-Origin', '*');
