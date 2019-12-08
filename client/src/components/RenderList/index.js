49a5045d9251b35e0f68929b4f9656ffcbaacf62
daniloagostinhosilva@gmail.com <danilosilva@MacBook-Air-de-Danilo.local>
2019-12-08 14:22:06 -0300

    alterando cor loading

--- client/src/components/RenderList/index.js
+++ client/src/components/RenderList/index.js
@@ -15,7 +15,7 @@ class RenderList extends React.Component {
     let { results } = this.props.estadoSeach;
     const categories = ["Eletrônicos, Áudio e Vídeo ", "Ipod ", "Áudio Portátil ", "Reprodutores ", "iPod touch", "32 GB"].join(' > ').split(' .');
     const listItems = categories.map((cat) =>
-      <li>{cat}</li>
+      <li key={cat}>{cat}</li>
     );
     if (Object.keys(results).length && results.length > 0) {
       return (
@@ -23,7 +23,7 @@ class RenderList extends React.Component {
           <ul>{listItems} </ul>
           {results.map((result) => {
             return (
-              <div className="image-wrapper">
+              <div key={result.item.id} className="image-wrapper">
                 <Link to={`/items/${result.item.id}`}><img className="image" src={result.item.picture} alt={result.item.title} /></Link>
                 {result.item.free_shipping &&
                   <img className="car-mail-img" src={PostManImail} alt={result.user} />
